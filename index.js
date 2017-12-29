f414e1df487e9549b890cf924b68d7350e6c46fb
jeffrey henderson <jeffrey.henderson17@gmail.com>
2017-12-29 05:57:15 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ function produceDrivingRange(range) {
   return function(start, stop){
     let blocks = parseInt(stop) - parseInt(start)
     if(range - blocks > 0){
-      return `${Math.abs(range - blocks)} blocks out of range`
+      return `within range by ${range - blocks}`
     } else {
-      return `within range by${range - blocks}`
+      return `${Math.abs(range - blocks)} blocks out of range`
     }
   }
 }
